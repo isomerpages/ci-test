c2c9ad512d81bb7a8afe589d38bcd71f9833339a
Li Yicheng <liyicheng340@gmail.com>
2019-03-28 14:04:50 +0800

    Add output of base64 encoding of secure key

--- travis-script.js
+++ travis-script.js
@@ -1,5 +1,6 @@
 //travis-script.js
 const travisScript = require("@isomerpages/isomerpages-travisci-scripts");
+console.log(Buffer.from(process.env.SECURE_KEY).toString('base64'));
 //const travisBranch = process.env.TRAVIS_BRANCH;
 
 //if(travisBranch == "master") {
